f8bf95bf6a5a6a3350e2ae474e7bb82ee19630d0
AbbazAdilxanov <qanatreymbaev33@gmail.com>
2023-06-07 16:13:45 +0300

    First

---
+++ lesson.1.py
@@ -0,0 +1,17 @@
+<h1 align="center">Hi 👋, I'm Abbaz Adilxanov</h1>
+<h3 align="center">A passionate python developer from I'm in Nukus</h3>
+
+- 📫 How to reach me email **abbaz3389@gmail.com**
+
+- ⚡ Fun fact **I'm the funny**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://instagram.com/95.samurayi" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="95.samurayi" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
+
+
